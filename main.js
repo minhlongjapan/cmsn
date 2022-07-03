4d48068238ba125fe0d9d72ef80fe82df3b4fc6a
minhlongjapan <96803487+minhlongjapan@users.noreply.github.com>
2022-07-03 21:39:44 +0900

    Update main.js

--- main.js
+++ main.js
@@ -136,3 +136,124 @@ after_close:function(){// モーダルが閉じた後に行う動作
     $('html').css('overflow-y','scroll');/*縦スクロールバーを出す*/
 }
 });
+
+
+
+
+function H1() {                
+  swal({
+      title: "Chúc Mừng Sinh Nhật Bích Ngọc", // Tiêu đề của popup
+      text: "Chúc em luôn thành đạt trong cuộc sống nhé!",
+      button: {
+          text: "❤️️",
+      },
+  });
+}
+function A1() {                
+  swal({
+      title: "Chúc Mừng Sinh Nhật Bích Ngọc", // Tiêu đề của popup
+      text: "Chúc em có thật nhiều điều may mắn và hạnh phúc trong thời gian sắp tới nhé!",
+      button: {
+          text: "❤️️",
+      },
+  });
+}
+function P1() {                
+  swal({
+      title: "Chúc Mừng Sinh Nhật Bích Ngọc", // Tiêu đề của popup
+      text: "Hãy sống thật hết mình với những đam mê để có một tuổi mới thật ý nghĩa nhé em!",
+      button: {
+          text: "❤️️",
+      },
+  });
+}
+function P2() {                
+  swal({
+      title: "Chúc Mừng Sinh Nhật Bích Ngọc", // Tiêu đề của popup
+      text: "Chúc em có một tương lai thật tươi sáng cùng với những điều tốt đẹp sắp đến nhé",
+      button: {
+          text: "❤️️",
+      },
+  });
+}
+function Y1() {                
+  swal({
+      title: "Chúc Mừng Sinh Nhật Bích Ngọc", // Tiêu đề của popup
+      text: "Vạn vật bỗng trở nên tươi đẹp và tràn đầy sức sống một cách thật kì lạ! Vì đây chính là ngày sinh nhật của em đấy!",
+      button: {
+          text: "❤️️",
+      },
+  });
+}
+function B() {                
+  swal({
+      title: "Chúc Mừng Sinh Nhật Bích Ngọc", // Tiêu đề của popup
+      text: "Hôm nay là một ngày tuyệt vời, ngày em đến với thế giới này trong sự chào đón và vui mừng của mọi người! \n Sinh nhật vui vẻ em nhé!",
+      button: {
+          text: "❤️️",
+      },
+  });
+}
+function I() {                
+  swal({
+      title: "Chúc Mừng Sinh Nhật Bích Ngọc", // Tiêu đề của popup
+      text: "Chúc mọi niềm vui sẽ đến với em để xua tan đi phiền muộn.",
+      button: {
+          text: "❤️️",
+      },
+  });
+}
+function R() {                
+  swal({
+      title: "Chúc Mừng Sinh Nhật Bích Ngọc", // Tiêu đề của popup
+      text: "Chúc em sinh nhật tràn ngập niềm vui và sự yêu thương!",
+      button: {
+          text: "❤️️",
+      },
+  });
+}
+function T() {                
+  swal({
+      title: "Chúc Mừng Sinh Nhật Bích Ngọc", // Tiêu đề của popup
+      text: "Chúc em sức khỏe dồi dào, thu hoạch thêm nhiều thành công rực rỡ!",
+      button: {
+          text: "❤️️",
+      },
+  });
+}
+function H2() {                
+  swal({
+      title: "Chúc Mừng Sinh Nhật Bích Ngọc", // Tiêu đề của popup
+      text: "Hãy làm cho ngày hôm nay trở thành một ngày sinh nhật thật đáng nhớ, chứa đầy những kỉ niệm, niềm vui và tiếng cười nhé!",
+      button: {
+          text: "❤️️",
+      },
+  });
+}
+function D() {                
+  swal({
+      title: "Chúc Mừng Sinh Nhật Bích Ngọc", // Tiêu đề của popup
+      text: "Chúc em có một ngày sinh nhật ý nghĩa! Đây sẽ là một ngày tuyệt vời cùng với những điều bất ngờ nhất và đặc biệt nhất của riêng em!",
+      button: {
+          text: "❤️️",
+      },
+  });
+}
+function A2() {                
+  swal({
+      title: "Chúc Mừng Sinh Nhật Bích Ngọc", // Tiêu đề của popup
+      text: "Sinh nhật chỉ kéo dài vỏn vẹn trong 24 giờ. Vậy hãy cố gắng cảm nhận nó một cách trọn vẹn nhất em nhé!",
+      button: {
+          text: "❤️️",
+      },
+  });
+}
+function Y2() {                
+  swal({
+      title: "Chúc Mừng Sinh Nhật Bích Ngọc", // Tiêu đề của popup
+      text: "Chúc em một tuổi mới thật đẹp, thật tuyệt vời với những điều mới lạ và thú vị trong cuộc sống!",
+      button: {
+          text: "❤️️",
+      },
+  });
+}
